b67523f1cf465e2f440ef3b0e9b8c173348ebb9f
pampukhaleo <lgpampukha@gmail.com>
2022-11-13 20:29:24 +0200

    blue circle for today date

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -5,12 +5,19 @@ import { days } from '../../utils/dateUtils.js';
 const Navigation = ({ weekDates }) => {
   return (
     <header className="calendar__header">
-      {weekDates.map((dayDate) => (
-        <div className="calendar__day-label day-label">
-          <span className="day-label__day-name">{days[dayDate.getDay()]}</span>
-          <span className="day-label__day-number">{dayDate.getDate()}</span>
-        </div>
-      ))}
+      {weekDates.map(dayDate => {
+        const dayNumberClassName =
+          new Date().getDate() === dayDate.getDate()
+            ? `day-label__day-number today-circle`
+            : 'day-label__day-number';
+
+        return (
+          <div className="calendar__day-label day-label">
+            <span className="day-label__day-name">{days[dayDate.getDay()]}</span>
+            <span className={dayNumberClassName}>{dayDate.getDate()}</span>
+          </div>
+        );
+      })}
     </header>
   );
 };
