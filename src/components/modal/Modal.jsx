14e36ab241a3a58577d5cdc9b09c38f4695fd889
pampukhaleo <lgpampukha@gmail.com>
2024-04-23 17:38:52 +0300

    Update modal styles and structure

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Modal = ({ onClose, children }) => (
-  <div>
+  <>
     <button className="create-event__close-btn" onClick={onClose}>
       +
     </button>
     {children}
-  </div>
+  </>
 );
 
 export default Modal;
